402b623a62f379d46c65fc8f42277ae29d3394f0
Alan Chodyniecki <alan.chodyniecki@softwire.com>
2022-09-09 17:57:35 +0100

    Provisionally have postcode lookup working

--- BusBoard/bus_board.rb
+++ BusBoard/bus_board.rb
@@ -2,8 +2,37 @@
 require 'uri'
 require 'net/http'
 require 'json'
+require 'erb'
 
 class TfLInterface
+
+
+
+  def get_all_arrivals(stop_ids)
+    all_arrivals = []
+    stop_ids.each do |stop_id|
+      arrival =  get_arrivals(stop_id)
+      all_arrivals += arrival
+    end
+    all_arrivals
+  end
+
+  def get_stop_ids(stop)
+    ids = []
+    stop["children"].each { |child| ids << child["id"] }
+    ids
+  end
+
+
+
+  def get_nearest_stops(lat, lon)
+    uri = get_nearest_stop_uri(lat, lon)
+    res = Net::HTTP.get_response(uri)
+    json_res = JSON.parse(res.body) if res.is_a?(Net::HTTPSuccess)
+    stops = json_res["stopPoints"]
+    stops.sort_by { |stop| stop["distance"] }
+    stops
+  end
 
   def get_arrivals(stop_id)
     uri = get_arrivals_uri(stop_id)
@@ -13,14 +42,55 @@ class TfLInterface
 
   private
 
+  @@bus_stop_types = ["NaptanBusCoachStation", "NaptanBusWayPoint", "NaptanHailAndRideSection", "NaptanOnstreetBusCoachStopCluster", "NaptanOnstreetBusCoachStopPair", "NaptanPublicBusCoachTram", "TransportInterchange"]
+
   def get_arrivals_uri(stop_id)
     URI("https://api.tfl.gov.uk/StopPoint/#{stop_id}/Arrivals")
   end
+
+  def get_nearest_stop_uri(lat, lon)
+    base_uri = URI("https://api.tfl.gov.uk/StopPoint")
+    params = {
+      :lat => lat,
+      :lon => lon,
+      :stopTypes => @@bus_stop_types.join(",")
+    }
+    base_uri.query = URI.encode_www_form(params)
+    base_uri
+  end
+
+
+end
+
+class PostcodeInterface
+
+  def get_postcode_lat_long(postcode)
+    uri = get_postcode_lat_long_uri(postcode)
+    res = Net::HTTP.get_response(uri)
+    json_res = (JSON.parse(res.body) if res.is_a?(Net::HTTPSuccess))["result"]
+    {
+      :lat => json_res["latitude"],
+      :lon => json_res["longitude"]
+    }
+  end
+
+  private
+
+  def get_postcode_lat_long_uri(postcode)
+
+    URI::HTTP.build(host: "api.postcodes.io", path: "/postcodes/#{ERB::Util::url_encode(postcode)}")
+  end
+
 end
 
 class CLI
   def get_stop_id
     puts "Please enter the stop id:"
+    gets.chomp
+  end
+
+  def get_postcode
+    puts "Please enter a postcode:"
     gets.chomp
   end
 
@@ -35,8 +105,26 @@ class CLI
 
 end
 
+
 cli = CLI.new
 tfl_interface = TfLInterface.new
-stop_id = cli.get_stop_id
-arrivals = tfl_interface.get_arrivals(stop_id)
-cli.print_next_5_arrivals(arrivals)
+postcode_interface = PostcodeInterface.new
+
+postcode = cli.get_postcode
+postcode_lat_lon = postcode_interface.get_postcode_lat_long(postcode)
+postcode_lat = postcode_lat_lon[:lat]
+postcode_lon = postcode_lat_lon[:lon]
+
+nearest_stops = tfl_interface.get_nearest_stops(postcode_lat, postcode_lon)
+
+nearest_stop = nearest_stops[0]
+nearest_stop_arrivals = tfl_interface.get_all_arrivals(tfl_interface.get_stop_ids(nearest_stop))
+
+puts nearest_stop["commonName"]
+cli.print_next_5_arrivals(nearest_stop_arrivals)
+
+second_nearest_stop = nearest_stops[1]
+second_nearest_stop_arrivals = tfl_interface.get_all_arrivals(tfl_interface.get_stop_ids(second_nearest_stop))
+
+puts second_nearest_stop["commonName"]
+cli.print_next_5_arrivals(second_nearest_stop_arrivals)
